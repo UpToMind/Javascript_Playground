cb7cb590f30161b6f069b37f52f1e330c2fda75c
이동준 <idongjun@idongjun-ui-MacBookPro.local>
2024-10-31 16:32:04 +0900

    add 2.2

---
+++ MOMENTUM/2.1 Basic Data Types/app.js
@@ -0,0 +1,7 @@
+// 숫자 타입
+console.log(2 + 2);
+console.log(223534 + 2.5);
+
+// 문자 타입
+console.log("hello");
+console.log("hello" + " my name is dj");
---
+++ MOMENTUM/2.3 const and let/app.js
@@ -0,0 +1,19 @@
+console.log(345546);
+console.log("sdfsfg");
+
+// 변수의 중요성
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 - 2);
+
+// 변수를 사용해보자
+const a = 5;
+const b = 2;
+const myName = "dj";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(a - b);
+console.log("hello" + myName);
