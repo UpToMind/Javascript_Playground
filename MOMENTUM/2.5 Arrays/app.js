b6926bc092c0ceecde75a48c112d2e6cbd4a6764
이동준 <lee19980424@gmail.com>
2024-10-31 17:09:05 +0900

    add 2.5

---
+++ MOMENTUM/2.5 Arrays/app.js
@@ -0,0 +1,21 @@
+// array
+
+// const mon = "mon";
+// const tue = "tue";
+// const wed = "wed";
+// const thu = "thu";
+// const fri = "fri";
+// const sat = "sat";
+// const sun = "sund";
+
+// const daysOfWeek = mon + tue + wed + thu + fri + sat + sun;
+
+// const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+console.log(daysOfWeek);
+console.log(daysOfWeek[5]);
+
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
