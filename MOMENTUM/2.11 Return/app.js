9f395d895e4b395999f01d09e0cffcf9f38c4011
이동준 <lee19980424@gmail.com>
2024-11-01 13:41:10 +0900

    add 2.11 return

---
+++ MOMENTUM/2.11 Return/app.js
@@ -0,0 +1,11 @@
+const calculator = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+
+console.log(calculator.add(1, 2));
+console.log(calculator.subtract(1, 2));
+console.log(calculator.multiply(1, 2));
+console.log(calculator.divide(1, 2));
