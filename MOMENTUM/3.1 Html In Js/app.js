1171377403ff53e5becaf5b9e9f0b5860794751a
이동준 <lee19980424@gmail.com>
2024-11-02 18:05:22 +0900

    add 3.1 Html In Js

---
+++ MOMENTUM/3.1 Html In Js/app.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("title");
+
+console.log(title);
+console.dir(title);
+
+title.innerHTML = "Hi! From JS";
+console.log(title.id);
+console.log(title.className);
